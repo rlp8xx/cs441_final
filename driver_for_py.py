04288002a709b8f00c298bda5643c555b25c26c5
Bobby Plubell <rlp8xx@mail.umkc.edu>
2020-05-07 19:31:17 -0500

    fast

---
+++ driver_for_py.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# This script runs the haskell executable and calculates average times
+
+import subprocess
+def run_with_threads(threads=1):
+    args = ['python3', 'final.py', "{}".format(threads)]
+    out = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    (stdout,stderr) = out.communicate()
+    stdout = [x for x in stdout.decode('utf-8').split("\n") if x]
+    time = stdout[len(stdout)-1]
+    return float(time.strip())
+
+trials = 3
+for threads in [1,2,4,8,16,32,64]:
+    total_time = 0
+    for trial in range(trials):
+        time = run_with_threads(threads)
+        total_time += time
+        print("Threads: {}, Trial: {}, Time: {}".format(threads,trial,time))
+    avg_time = total_time / trials
+    print("Average time over {} trials for {} threads is {}".format(trials, threads, avg_time))
